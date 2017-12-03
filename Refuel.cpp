a68828e2937ee4b74a86c9f1855492623df47db6
erinbev <34074648+erinbev@users.noreply.github.com>
2017-12-03 11:08:32 -0500

    Update Refuel.cpp

--- Refuel.cpp
+++ Refuel.cpp
@@ -12,3 +12,7 @@ void refuel(LinkedList::Node* plane) {
 	onGround.deleteElement(plane->id);
 	atRefuel.addElementBack(temp);
 }
+
+
+
+// Comment from Erin: doesn't it take 10 time units to refuel, not 20?
