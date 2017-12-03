5a1a55c9e29753de4cfa3ab23ec2dc3d3e5c508e
Nikhil <nikn86@gmail.com>
2017-12-03 12:40:10 -0500

    Update land.cpp

--- land.cpp
+++ land.cpp
@@ -3,6 +3,7 @@ void land(plane) {
 	numPplSafe += plane->people;
 	numGCSafe += plane->grandChild;
 	cargeSafe += plane->cargo;
-
+	numAirWait+= plane-> waitTime
+	totalGCWait += numAirWait * (plane -> grandChild);
 	inAir.deleteElement(plane -> id);
 }
