a426d2838a7d70c9611d67b7311e01a51cb43c0e
donavank <DonavanKeen@live.com>
2017-12-03 12:28:36 -0500

    Update Refuel.cpp

--- Refuel.cpp
+++ Refuel.cpp
@@ -1,15 +1,5 @@
 void refuel(LinkedList::Node* plane) {
-	LinkedList::Node* temp = new LinkedList::Node();
-	temp->id = plane->id;
-	temp->action = plane->action;
-	temp->refueling = 10;
-	temp->cargo = plane->cargo;
-	temp->waitTime = plane->waitTime;
-	temp->fuelCap = plane->fuelCap;
-	temp->grandChild = plane->grandChild;
-	temp->fuel = plane->fuel;
-	temp->next = nullptr;
-	temp->prev = nullptr;
+	LinkedList::Node* temp = new LinkedList::Node(plane);
 	onGround.deleteElement(plane->id);
 	atRefuel.addElementBack(temp);
 }
